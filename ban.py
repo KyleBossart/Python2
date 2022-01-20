eaab8064b3fc1abd9d90ace5118a5f85ef047e4e
KyleBossart <70309924+KyleBossart@users.noreply.github.com>
2022-01-20 12:30:46 -0700

    Update ban.py

--- ban.py
+++ ban.py
@@ -4,7 +4,7 @@
 
 # December 15, 2020
 
-# A simple python script that allows user to change Bandit level by running instead of having to manually change the Bandit level each time. Now instead of holding back arrow for 3 seconds and hitting three keys, I can type out a command. yay.
+# A simple python script that allows user to change Bandit level by running this instead of having to manually change the Bandit level each time. Now instead of holding back arrow for 3 seconds and hitting three keys, I can type out a command. yay.
 #-----------------------------------------------------------------------------------------------------------------------------------------------
 
 # Imports "os" which allows ban.py to use the os.system command below to output the text into bash.
